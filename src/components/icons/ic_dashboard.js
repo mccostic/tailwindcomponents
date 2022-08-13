f1131b48b10965f7caeb6b6db5fd45f5d9e2006e
Senninseyi <seyi.oyebamiji@gmail.com>
2022-08-13 09:09:51 +0100

    sidebar links icons

---
+++ src/components/icons/ic_dashboard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Ic_Dashboard = ({width, height}) => {
+  return (
+    <>
+      <svg
+        id="dashboard"
+        xmlns="http://www.w3.org/2000/svg"
+        width={width}
+        height={height}
+        viewBox="0 0 48 48"
+      >
+        <path
+          id="Path_17"
+          dataname="Path 17"
+          d="M8,26H20a2.006,2.006,0,0,0,2-2V8a2.006,2.006,0,0,0-2-2H8A2.006,2.006,0,0,0,6,8V24A2.006,2.006,0,0,0,8,26ZM6,40a2.006,2.006,0,0,0,2,2H20a2.006,2.006,0,0,0,2-2V32a2.006,2.006,0,0,0-2-2H8a2.006,2.006,0,0,0-2,2Zm20,0a2.006,2.006,0,0,0,2,2H40a2.006,2.006,0,0,0,2-2V26a2.006,2.006,0,0,0-2-2H28a2.006,2.006,0,0,0-2,2Zm2-20H40a2.006,2.006,0,0,0,2-2V8a2.006,2.006,0,0,0-2-2H28a2.006,2.006,0,0,0-2,2V18A2.006,2.006,0,0,0,28,20Z"
+          fill="#fff"
+        />
+        <path id="Path_18" dataname="Path 18" d="M0,0H48V48H0Z" fill="none" />
+      </svg>
+    </>
+  );
+};
